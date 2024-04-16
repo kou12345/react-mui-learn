3eec16fa5f3fc143e1471b34b3817e1ee1d38f47
kou12345 <hi.0412.ko.2002@gmail.com>
2024-04-16 12:44:39 +0900

    add EnhancedTable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,7 @@
 "use client";
 
-import { Box, Button, Stack } from "@mui/material";
+import { EnhancedTable } from "@/components/EnhancedTable";
 
 export default function Home() {
-  return (
-    <div>
-      <Stack
-        spacing={2}
-        sx={{
-          marginBottom: "40px",
-        }}
-      >
-        <textarea
-          name=""
-          id=""
-          style={{
-            height: "300px",
-            resize: "none",
-          }}
-        ></textarea>
-        <textarea
-          name=""
-          id=""
-          style={{
-            height: "900px",
-            // resize: "none",
-          }}
-        ></textarea>
-      </Stack>
-
-      <Box
-        height={60}
-        sx={{
-          display: "flex",
-          justifyContent: "flex-end",
-          position: "fixed",
-          bottom: 0,
-          left: 0,
-          right: 0,
-          backgroundColor: "silver",
-        }}
-      >
-        <Button
-          variant="contained"
-          color="primary"
-          size="small"
-          sx={{
-            marginX: "6px",
-            marginY: "6px",
-          }}
-        >
-          Primary
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-          size="small"
-          sx={{
-            marginX: "6px",
-            marginY: "6px",
-          }}
-        >
-          Secondary
-        </Button>
-      </Box>
-    </div>
-  );
+  return <EnhancedTable />;
 }
